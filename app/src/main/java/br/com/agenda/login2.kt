53b224b12213e2fb99ddb639757772177117062b
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-11-25 11:22:07 -0300

    Initial commit

---
+++ app/src/main/java/br/com/agenda/login2.kt
@@ -0,0 +1,11 @@
+package br.com.agenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+    }
+}
