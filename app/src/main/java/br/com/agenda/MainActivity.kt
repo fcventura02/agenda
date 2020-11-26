45c84abaf213f30963e2ceb62e2f0af714e3c685
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-11-26 15:16:29 -0300

    created landing page and login layout

--- app/src/main/java/br/com/agenda/MainActivity.kt
+++ app/src/main/java/br/com/agenda/MainActivity.kt
@@ -1,11 +1,16 @@
 package br.com.agenda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_principal.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login2)
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this, login2::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/br/com/agenda/login2.kt
+++ app/src/main/java/br/com/agenda/login2.kt
@@ -1,11 +1,32 @@
 package br.com.agenda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login2.*
 
 class login2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+
+
+
+        btm_login.setOnClickListener {
+            if (checkLogin(et_login_email.text.toString(), et_login_password.text.toString())) {
+                val intent = Intent(this, principal::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun checkLogin(login: String, password: String): Boolean{
+        val email = "teste@teste.com"
+        val senha = "123456"
+
+        if (login != email || password != senha){
+            return false
+        }
+        return true
     }
 }
